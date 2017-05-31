40cf103466f2667ed1523f3183b7117dc0742bd0
Enngage <ennspassion@gmail.com>
2017-05-31 14:09:12 +0200

    removes dependencies

--- src/app/modules/items/items.module.ts
+++ src/app/modules/items/items.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 import { GetItemsComponent } from './get-items.component';
 import { ItemsRouter } from './items.router';
 
-import { MarkdownModule } from 'angular2-markdown';
-
 @NgModule({
     imports: [
         ItemsRouter,
         CommonModule,
-        MarkdownModule
     ],
     declarations: [
         GetItemsComponent,
