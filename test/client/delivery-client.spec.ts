5d479d84947379dcc6bdf56d363598f50eed88ba
Enngage <ennspassion@gmail.com>
2017-08-08 14:40:11 +0200

    test commnit - travis build

--- test/client/delivery-client.spec.ts
+++ test/client/delivery-client.spec.ts
@@ -3,7 +3,6 @@ import { DeliveryClient, DeliveryClientConfig } from '../../lib';
 
 // tests
 describe('Delivery Client initialization', () => {
-
   it(`initialization DeliveryClient without config should throw error`, () => {
     expect(() => new DeliveryClient(null)).toThrowError();
   });
