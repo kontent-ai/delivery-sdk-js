76c1b37c791dd5490463bca0b7168dd45c88491f
Enngage <ennspassion@gmail.com>
2017-08-08 15:03:20 +0200

    travis test #1

---
+++ karma-travis.conf.js
@@ -0,0 +1,53 @@
+module.exports = function (config) {
+    config.set({
+        frameworks: ["jasmine", "karma-typescript"],
+        plugins: [
+            require("karma-jasmine"),
+            require("karma-typescript"),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage'),
+        ],
+        files: [
+            { pattern: "lib/**/*.ts" },
+            { pattern: "test/**/*.ts" }
+        ],
+        exclude: [
+        ],
+        preprocessors: {
+            "lib/**/*.ts": ["karma-typescript", "coverage"],
+            "test/**/*.ts": ["karma-typescript"]
+        },
+        reporters: ["kjhtml", "progress",],
+        customLaunchers: {
+            // chrome setup for travis CI using chromium
+            Chrome_travis_ci: {
+                base: "Chrome",
+                flags: [" — no-sandbox"]
+            }
+        },
+        browsers: ["Chrome"],
+        karmaTypescriptConfig: {
+            bundlerOptions: {
+                transforms: [require("karma-typescript-es6-transform")()]
+            },
+            compilerOptions: {
+                "module": "commonjs",
+                "target": "es5",
+                "lib": ["es2015", "es2017", "dom"],
+                "exclude": [
+                    "node_modules"
+                ]
+            },
+        },
+        port: 9669,
+        colors: true,
+        autoWatch: false,
+        browsers: ["Chrome"],
+        singleRun: true,
+        client: {
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        logLevel: config.LOG_ERROR,
+    });
+};
