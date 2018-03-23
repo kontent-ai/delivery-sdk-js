bd3cf1d083b8e70d489707b6f0fe6848c9ab9a83
Enngage <ennspassion@gmail.com>
2018-03-23 14:07:15 +0100

    Removes plugin

--- webpack.config.js
+++ webpack.config.js
@@ -32,12 +32,6 @@ module.exports = {
             // fixes WARNING Critical dependency: the request of a dependency is an expression
             /(.+)?parse5(\\|\/)(.+)?/,
             path.join(__dirname, 'lib')
-        ),
-        new webpack.ContextReplacementPlugin(
-            // workaround for Parse5
-            // fixes WARNING Critical dependency: the request of a dependency is an expression
-            /(.+)?parse5(\\|\/)(.+)?/,
-            path.join(__dirname, '_bundles')
         )
     ]
 };
