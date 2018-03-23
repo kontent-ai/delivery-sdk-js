9d352da3cece3057e4a0bca2643d5e6f7aa327b4
Enngage <ennspassion@gmail.com>
2018-03-23 13:53:02 +0100

    new build setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    entry: './lib/index.ts',
+    entry: {
+        'index': './lib/index.ts',
+    },
     resolve: { extensions: ['.ts', '.js'] },
     output: {
         // Puts the output at the root of the dist folder
-        path: path.join(__dirname, 'dist'),
-        filename: 'index.js'
+        path: path.join(__dirname, '_bundles'),
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
     },
+    devtool: 'source-map',
     module: {
         rules: [
             {
-                test: /\.tsx?$/, loader: 'ts-loader',
+                test: /\.ts$/, loader: 'ts-loader',
                 include: [
                     path.resolve(__dirname, "lib"), // include only library
                 ],
             },
-            {
-                test: /\.js$/, loader: 'source-map-loader',
-                include: [
-                    path.resolve(__dirname, "lib"), // include only library
-                ],
-            },
+
         ]
     },
     performance: { hints: false }, // this disables warning about large output file (in our case its ~300Kb which is fine)
