e6d1cd59750e84137d9c5808171a927e1dd5ac7b
Enngage <ennspassion@gmail.com>
2018-04-09 14:35:03 +0200

    Refactors main index barrel export

--- lib/index.ts
+++ lib/index.ts
@@ -1,60 +1,33 @@
-/**
- * Public API
- */
-
-// core
+/* client */
 export * from './client';
-export * from './config';
-export * from './models/common/sort-order.enum';
-export * from './interfaces/common/iquery.config';
-export * from './interfaces/common/isdk-info.class';
-export * from './interfaces/common/iheader.interface';
 
-// data contracts
+/* config */
+export * from './config';
+
+/* data contracts */
 export * from './data-contracts';
 
-// fields
+/* fields */
 export * from './fields';
 
-// parser
+/* parser */
 export * from './parser';
 
-// resolvers
+/* resolvers */
 export * from './resolvers';
 
-// services
+/* services */
 export * from './services';
 
-// mapp
+/* mappers */
 export * from './mappers';
 
-// filters
-export * from './models/common/filters';
-
-// queries
+/* queries */
 export * from './query';
 
-// parameters
-export * from './models/common/parameters';
+/* models */
+export * from './models';
 
-// items
-export * from './models/item/type-resolver.class';
-export * from './models/item/content-item.class';
-export * from './models/item/responses';
-export * from './models/item/content-item-system-attributes';
-export * from './models/item/link.class';
+/* interfaces */
+export * from './interfaces';
 
-// type
-export * from './models/type/content-type.class';
-export * from './models/type/responses';
-
-// taxonomy
-export * from './models/taxonomy/responses';
-export * from './models/taxonomy/taxonomy-group.class';
-export * from './models/taxonomy/taxonomy-system-attributes.class';
-export * from './models/taxonomy/taxonomy-terms.class';
-
-// element
-export * from './models/element/responses';
-export * from './models/element/element.class';
-export * from './models/element/element-option.class';
