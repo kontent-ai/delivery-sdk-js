5be254185e0e4c7eef51c29b1c6594fedf2a494e
Enngage <ennspassion@gmail.com>
2018-05-02 09:46:58 +0200

    Up version

--- lib/sdk-info.generated.ts
+++ lib/sdk-info.generated.ts
@@ -3,6 +3,6 @@ import { ISDKInfo } from './interfaces';
 
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '4.0.0-beta.5',
+    version: '4.0.0-beta.6',
     name: 'kentico-cloud-delivery-typescript-sdk'
 };
