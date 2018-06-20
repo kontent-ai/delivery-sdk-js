7f3ec84d875dee6456add206b15932512e30eb83
Enngage <ennspassion@gmail.com>
2018-06-20 08:45:31 +0200

    Up version

--- packages/delivery/lib/sdk-info.generated.ts
+++ packages/delivery/lib/sdk-info.generated.ts
@@ -3,6 +3,6 @@ import { ISDKInfo } from './interfaces';
 
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '4.0.3',
+    version: '4.0.4',
     name: 'kentico-cloud-delivery'
 };
