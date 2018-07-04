dc276542d11706aded4248e3900211ba2a641778
Enngage <ennspassion@gmail.com>
2018-07-04 12:41:33 +0200

    Fix node test imports

--- packages/delivery/test-node/official/view-content-item.js
+++ packages/delivery/test-node/official/view-content-item.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const KenticoCloud = require('../../_commonjs');
+const KenticoCloud = require('../../_commonjs/lib');
 
 class Article extends KenticoCloud.ContentItem {
     constructor() {
--- packages/delivery/test-node/rich-text/live-rich-text.js
+++ packages/delivery/test-node/rich-text/live-rich-text.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const KenticoCloud = require('../../_commonjs');
+const KenticoCloud = require('../../_commonjs/lib');
 
 class Actor extends KenticoCloud.ContentItem {
 
