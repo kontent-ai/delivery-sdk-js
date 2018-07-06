065f1661b5648a6c922c35820cba04cdd8b26886
Enngage <ennspassion@gmail.com>
2018-07-06 14:51:13 +0200

    Upds tracking version & scripts

--- packages/tracking/lib/sdk-info.generated.ts
+++ packages/tracking/lib/sdk-info.generated.ts
@@ -3,6 +3,6 @@ import { ISDKInfo } from './models';
 
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '1.0.0',
+    version: '1.1.0',
     name: 'kentico-cloud-tracking'
 };
