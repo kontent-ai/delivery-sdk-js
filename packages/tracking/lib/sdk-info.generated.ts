81e1cb42faf16ac913fec14463b43bd81ff7b6c1
Enngage <ennspassion@gmail.com>
2018-08-13 12:23:39 -0400

    Tracking minor

--- packages/tracking/lib/sdk-info.generated.ts
+++ packages/tracking/lib/sdk-info.generated.ts
@@ -3,6 +3,6 @@ import { ISDKInfo } from './models';
 
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '1.1.1',
+    version: '1.2.0',
     name: 'kentico-cloud-tracking'
 };
