be5fd12e0684c517aa68a0119f0cffce1846e121
Enngage <ennspassion@gmail.com>
2018-10-10 12:26:20 +0200

    delivery beta 5

--- packages/delivery/lib/sdk-info.generated.ts
+++ packages/delivery/lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from 'kentico-cloud-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '5.0.0-beta.4',
+    version: '5.0.0-beta.5',
     name: 'kentico-cloud-delivery'
 };
