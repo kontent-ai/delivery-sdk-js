6f39656c03c6492a093fa30cb4f0cec4dea641ec
Enngage <ennspassion@gmail.com>
2019-01-08 10:53:25 +0100

    Ups sdk info

--- packages/delivery/lib/sdk-info.generated.ts
+++ packages/delivery/lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from 'kentico-cloud-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '5.2.1',
+    version: '5.2.2',
     name: 'kentico-cloud-delivery'
 };
