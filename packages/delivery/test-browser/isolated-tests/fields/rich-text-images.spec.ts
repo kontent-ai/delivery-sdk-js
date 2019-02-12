b104063b3fb55a899776eccaae7666fcab9c7f00
Enngage <ennspassion@gmail.com>
2019-02-12 15:23:50 +0100

    Fixes and simplifies rich text field image test

--- packages/delivery/test-browser/isolated-tests/fields/rich-text-images.spec.ts
+++ packages/delivery/test-browser/isolated-tests/fields/rich-text-images.spec.ts
@@ -1,82 +1,38 @@
 import {
-    ContentItem,
-    ContentItemSystemAttributes,
     Fields,
+    getParserAdapter,
     IDeliveryClientConfig,
-    Link,
+    ImageUrlBuilder,
+    RichTextImage,
     richTextResolver,
     TypeResolver,
-    urlSlugResolver,
-    getParserAdapter,
-    ILinkResolverResult,
-    RichTextImage,
-    ImageUrlBuilder
 } from '../../../lib';
 
-class ActorMock extends ContentItem {
-    firstName!: Fields.TextField;
-    system!: ContentItemSystemAttributes;
-    url!: Fields.UrlSlugField;
-
-    constructor() {
-        super();
-    }
-
-    setProperties(id: string, codename: string, firstName: string) {
-        this.firstName = new Fields.TextField('firstName', firstName);
-        this.system = new ContentItemSystemAttributes({
-            id: id,
-            name: 'name',
-            codename: codename,
-            type: 'actor',
-            sitemapLocations: [],
-            language: 'en',
-            lastModified: new Date()
-        });
-
-        this.url = new Fields.UrlSlugField('name', codename, {
-            resolveLink: () => urlSlugResolver.resolveUrl({
-                fieldName: 'name',
-                type: 'type',
-                item: this,
-                linkResolver: (link: Link) => <ILinkResolverResult>{
-                    asHtml: `<test>${link.urlSlug}</test>`,
-                },
-                enableAdvancedLogging: true,
-                fieldValue: codename
-            })
-        });
-    }
-}
-
 describe('RichTextField with Images', () => {
-    // prepare config & type resolver
-    const typeResolvers: TypeResolver[] = [
-        new TypeResolver('actor', () => new ActorMock())
-    ];
-
     const config: IDeliveryClientConfig = {
-        projectId: '',
-        typeResolvers: typeResolvers
+        projectId: 'xxx',
+        typeResolvers: []
     };
 
     // prepare images
     const images: RichTextImage[] = [
         new RichTextImage({
-            imageId: '14mio',
-            url: 'www.domain.com/image1.png'
+            imageId: 'image1',
+            url: 'www.domain.com/image1.png',
+            description: ''
         }),
         new RichTextImage({
             imageId: 'image2',
-            url: 'www.domain.com/image2.png'
+            url: 'www.domain.com/image2.png',
+            description: ''
         })
     ];
 
     const image1 = images[0];
     const image2 = images[1];
 
-    const image1Html = getImageHtml(image1.imageId, image1.url, image1.description);
-    const image2Html = getImageHtml(image2.imageId, image2.url, image2.description);
+    const image1Html = getImageHtml('assetId1', image1.imageId, image1.url, image1.description);
+    const image2Html = getImageHtml('assetId2', image2.imageId, image2.url, image2.description);
 
     const html = `
     Testing html with images. ${image1Html} and ${image2Html}`;
@@ -97,7 +53,7 @@ describe('RichTextField with Images', () => {
                     richTextImageResolver: undefined
                 },
             }),
-            images: []
+            images: images
         });
 
         const expectedHtml1 = image1Html;
@@ -110,7 +66,7 @@ describe('RichTextField with Images', () => {
     });
 
     it(`Checks that images are resolved using custom resolver`, () => {
-        const field = new Fields.RichTextField('name', html, [], {
+        const field2 = new Fields.RichTextField('name', html, [], {
             links: [],
             resolveHtml: () => richTextResolver.resolveHtml(html, 'name', {
                 enableAdvancedLogging: false,
@@ -127,27 +83,30 @@ describe('RichTextField with Images', () => {
                         const newImageUrl = new ImageUrlBuilder(image.url)
                             .withCustomParam('xParam', 'xValue')
                             .getUrl();
-
                         return {
                             url: newImageUrl
                         };
-                    })
+                    }),
                 },
             }),
-            images: []
+            images: images
         });
 
-        const expectedHtml1 = getImageHtml(image1.imageId, image1.url + '?xParam=xValue', image1.description);
-        const expectedHtml2 = getImageHtml(image2.imageId, image2.url + '?xParam=xValue', image2.description);
+        const expectedHtml1 = getImageSrcHtml(image1.url + '?xParam=xValue');
+        const expectedHtml2 = getImageSrcHtml(image2.url + '?xParam=xValue');
 
-        const resultHtml = field.getHtml();
+        const resultHtml = field2.getHtml();
 
         expect(resultHtml).toContain(expectedHtml1);
         expect(resultHtml).toContain(expectedHtml2);
     });
 
-    function getImageHtml(imageId: string, imageUrl: string, description?: string): string {
-        return `<figure data-image-id=\"${imageId}\"><img src=\"${imageUrl}" alt=\"${description}\" data-image-id=\"${imageId}\"></figure>`;
+    function getImageSrcHtml(imageUrl: string): string {
+        return `src="${imageUrl}"`;
+    }
+
+    function getImageHtml(assetId: string, imageId: string, imageUrl: string, description?: string): string {
+        return `<figure data-asset-id="${assetId}" data-image-id="${imageId}"><img src="${imageUrl}" data-asset-id="${assetId}" data-image-id="${imageId}" alt="${description ? description : ''}"></figure>`;
     }
 });
 
