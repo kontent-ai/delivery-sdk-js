5d252d98eb894d3832a61ac964fff249c6a78852
Enngage <ennspassion@gmail.com>
2019-02-25 10:41:25 +0100

    renames fields file

--- packages/delivery/lib/fields/index.ts
+++ packages/delivery/lib/fields/index.ts
@@ -2,5 +2,5 @@ export * from './field-decorators';
 export * from './field-contracts';
 export * from './field-models';
 export * from './field-type';
-export * from './field-types';
+export * from './fields';
 export * from './field-resolver';
