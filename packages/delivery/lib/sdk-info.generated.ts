993756f34c5313c8df7ee1056867eced6b109a12
Enngage <ennspassion@gmail.com>
2019-02-26 08:16:49 +0100

    delivery minor

--- packages/delivery/lib/sdk-info.generated.ts
+++ packages/delivery/lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from 'kentico-cloud-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '5.5.0',
+    version: '5.6.0',
     name: 'kentico-cloud-delivery'
 };
