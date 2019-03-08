ba4edf4d14b8b19af7c2938ea37c247f0db4b694
Enngage <ennspassion@gmail.com>
2019-03-08 15:21:04 +0100

    delivery patch

--- packages/delivery/lib/sdk-info.generated.ts
+++ packages/delivery/lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from 'kentico-cloud-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '5.7.1',
+    version: '5.7.2',
     name: 'kentico-cloud-delivery'
 };
