1d9f3ff55fedd89657b6e6f707adec051567091e
Enngage <ennspassion@gmail.com>
2019-03-19 09:01:00 +0100

    CM patch and docs update

--- packages/content-management/lib/sdk-info.generated.ts
+++ packages/content-management/lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from 'kentico-cloud-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '0.0.4',
+    version: '0.0.5',
     name: 'kentico-cloud-content-management'
 };
