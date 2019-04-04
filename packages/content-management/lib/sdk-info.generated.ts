1da0276cba8ccb92ddc2c4a1ad7cb0a95964410a
Enngage <ennspassion@gmail.com>
2019-04-04 12:21:46 +0200

    Updates dependency of kentico-cloud-core to 1.4.5

--- packages/content-management/lib/sdk-info.generated.ts
+++ packages/content-management/lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from 'kentico-cloud-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '0.0.11',
+    version: '0.0.12',
     name: 'kentico-cloud-content-management'
 };
