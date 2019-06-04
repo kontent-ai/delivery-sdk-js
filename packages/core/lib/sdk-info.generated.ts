491b71bcf1e8ddee3434c7cc30a46f9ef1dfea9a
Enngage <ennspassion@gmail.com>
2019-06-04 14:16:29 +0200

    axios & dependency update + version update

--- packages/core/lib/sdk-info.generated.ts
+++ packages/core/lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from '.';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '1.5.0',
+    version: '1.6.0',
     name: 'kentico-cloud-core'
 };
--- packages/delivery/lib/sdk-info.generated.ts
+++ packages/delivery/lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from 'kentico-cloud-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '6.0.0-beta.9',
+    version: '6.0.0-beta.10',
     name: 'kentico-cloud-delivery'
 };
