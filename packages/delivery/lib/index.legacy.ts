677c2b906d06cd3abe901a4899caa08abee91343
Enngage <ennspassion@gmail.com>
2019-07-17 10:16:06 +0200

    Reduces polyfills for legacy bundle

--- packages/delivery/lib/index.legacy.ts
+++ packages/delivery/lib/index.legacy.ts
@@ -1,22 +1,16 @@
 // Polyfills for IE9, IE10 & IE11
+
 import 'es6-symbol/implement';
 import 'es6-promise/auto';
 import 'core-js/es/symbol';
 import 'core-js/es/object';
 import 'core-js/es/function';
-import 'core-js/es/parse-int';
-import 'core-js/es/parse-float';
 import 'core-js/es/number';
-import 'core-js/es/math';
 import 'core-js/es/string';
 import 'core-js/es/date';
 import 'core-js/es/array';
-import 'core-js/es/array';
-import 'core-js/es/regexp';
 import 'core-js/es/map';
 import 'core-js/es/set';
-import 'core-js/es/weak-map';
-import 'core-js/es/weak-set';
 
 // Public API
 export * from './client';
