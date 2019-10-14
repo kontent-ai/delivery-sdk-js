65aabf75a516ab89128d2bc80e43192cdf44f858
Enngage <ennspassion@gmail.com>
2019-10-14 12:28:53 +0200

    adds standard-release & fixes set-sdk-version scripts

--- misc/set-sdk-version.js
+++ misc/set-sdk-version.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const appVersion = require('../package.json').version;
 const packageName = require('../package.json').name;
-const versionHelper = require('../../core/misc/versionHelper');
+const versionHelper = require('./version-helper');
 
 versionHelper.createSdkVersionFile(
     path.join(__dirname + '/../lib/sdk-info.generated.ts'),
--- misc/verify-sdk-version.ts
+++ misc/verify-sdk-version.ts
@@ -1,7 +1,7 @@
 import { sdkInfo } from '../lib/sdk-info.generated';
 declare function require(name: string): any;
 
-const versionHelper = require('../../core/misc/versionHelper');
+const versionHelper = require('./version-helper');
 const appVersion = require('../package.json').version;
 
 versionHelper.verifySdkVersion(sdkInfo, appVersion);
