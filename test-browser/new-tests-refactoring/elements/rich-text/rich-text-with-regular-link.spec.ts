7be17a1211dc927c030e8af82af04a6aa0508ca2
Enngage <ennspassion@gmail.com>
2020-04-01 09:13:45 +0200

    internal: unifies parse5 & browser testing in rich text element tests

--- test-browser/new-tests-refactoring/elements/rich-text/rich-text-with-regular-link.spec.ts
+++ test-browser/new-tests-refactoring/elements/rich-text/rich-text-with-regular-link.spec.ts
@@ -1,13 +1,56 @@
-import { ContentItem, Elements, getParserAdapter, IUrlSlugResolverResult, richTextResolver, ItemUrlSlugResolver } from '../../../../lib';
+import { ContentItem, Elements, IUrlSlugResolverResult, richTextResolver, ItemUrlSlugResolver, BrowserRichTextParser } from '../../../../lib';
+import { Parse5RichTextParser } from '../../../../lib/parser/adapters/parse5-rich-text.parser';
 
 describe('Rich text with regular link', () => {
     const linkedItems: ContentItem[] = [];
-    const getLinkedItem: (codename: string) => ContentItem | undefined = (codename) => linkedItems.find(m => m.system.codename === codename);
+    const getLinkedItem: (codename: string) => ContentItem | undefined = codename =>
+        linkedItems.find(m => m.system.codename === codename);
     const getGlobalUrlSlugResolver: (type: string) => ItemUrlSlugResolver | undefined = type => undefined;
 
     const html = `Text with <a href="domain.com">link</a>`;
 
-    it(`Resolving HTML should succeed`, () => {
+    it(`Resolving HTML should succeed (parse5)`, () => {
+        const element = new Elements.RichTextElement(
+            {
+                rawElement: {
+                    name: 'name',
+                    taxonomy_group: undefined,
+                    type: 'x',
+                    value: html
+                },
+                contentItemSystem: {} as any,
+                propertyName: 'name'
+            },
+            linkedItems.map(m => m.system.codename),
+            {
+                links: [],
+                resolveRichTextFunc: () =>
+                    richTextResolver.resolveData('', html, 'name', {
+                        enableAdvancedLogging: false,
+                        getGlobalUrlSlugResolver: getGlobalUrlSlugResolver,
+                        links: [],
+                        getLinkedItem: getLinkedItem,
+                        images: [],
+                        richTextHtmlParser: new Parse5RichTextParser(),
+                        linkedItemWrapperClasses: ['kc-wrapper-class'],
+                        linkedItemWrapperTag: 'kc-item-wrapper',
+                        queryConfig: {
+                            richTextResolver: undefined as any,
+                            urlSlugResolver: link =>
+                                <IUrlSlugResolverResult>{
+                                    html: `<test>${link.urlSlug}</test>`
+                                }
+                        }
+                    }),
+                images: []
+            }
+        );
+
+        const expectedHtml = `Text with <a href="domain.com">link</a>`;
+        expect(element.resolveHtml()).toContain(expectedHtml);
+    });
+
+    it(`Resolving HTML should succeed (browser)`, () => {
         const element = new Elements.RichTextElement({
             rawElement: {
                 name: 'name',
@@ -25,7 +68,7 @@ describe('Rich text with regular link', () => {
                     links: [],
                     getLinkedItem: getLinkedItem,
                     images: [],
-                    richTextHtmlParser: getParserAdapter(),
+                    richTextHtmlParser: new BrowserRichTextParser(),
                     linkedItemWrapperClasses: ['kc-wrapper-class'],
                     linkedItemWrapperTag: 'kc-item-wrapper',
                     queryConfig: {
@@ -42,4 +85,3 @@ describe('Rich text with regular link', () => {
         expect(element.resolveHtml()).toContain(expectedHtml);
     });
 });
-
