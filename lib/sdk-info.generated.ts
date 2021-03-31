b32d1afbbd561e6b999c42f39d72b3ef4a873c38
Enngage <ennspassion@gmail.com>
2021-03-31 10:10:50 +0200

    release

--- lib/sdk-info.generated.ts
+++ lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from '@kentico/kontent-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '10.4.0',
+    version: '10.4.1',
     name: '@kentico/kontent-delivery'
 };
