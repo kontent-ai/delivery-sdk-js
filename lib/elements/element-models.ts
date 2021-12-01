8a778cf238368afe461f4582b613f63f876d4319
Enngage <ennspassion@gmail.com>
2021-12-01 09:17:25 +0100

    Makes rendition optional

--- lib/elements/element-models.ts
+++ lib/elements/element-models.ts
@@ -69,9 +69,9 @@ export namespace ElementModels {
         height?: number;
 
         /**
-         * Dictionary with rendition preset codenames as keys and respective renditions as values
+         * Dictionary with rendition preset codenames as keys and respective renditions as values.
          */
-        renditions: { [renditionPresetCodename: string]: Rendition };
+        renditions?: { [renditionPresetCodename: string]: Rendition };
     }
 
     export interface Rendition {
