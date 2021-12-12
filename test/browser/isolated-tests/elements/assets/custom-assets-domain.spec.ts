c8e70e402a3b81fcdf965c68fd51842e39958bef
Richard Sustek <ennspassion@gmail.com>
2021-12-12 15:43:00 +0100

    feat: Adds support for custom asset domains (fixes https://github.com/Kentico/kontent-delivery-sdk-js/issues/333)

---
+++ test/browser/isolated-tests/elements/assets/custom-assets-domain.spec.ts
@@ -0,0 +1,46 @@
+import { deliveryUrlHelper } from '../../../../../lib';
+import { getDeliveryClientWithJson, Movie } from '../../../setup';
+import * as responseJson from '../../fake-data/fake-warrior-response.json';
+
+describe('Custom assets domain', () => {
+    let item: Movie;
+    const customDomain: string = 'https://custom.com';
+
+    beforeAll(async () => {
+        const response = await getDeliveryClientWithJson(responseJson, {
+            assetsDomain: customDomain,
+            projectId: 'x'
+        })
+            .item<Movie>('xx')
+            .toPromise();
+
+        item = response.data.item;
+
+        console.log(item);
+    });
+
+    it(`Custom asset domain should be set in asset element`, () => {
+        const assetElement = item.elements.poster.value[0];
+
+        expect(assetElement.url).toEqual(
+            `${customDomain}/da5abe9f-fdad-4168-97cd-b3464be2ccb9/22504ba8-2075-48fa-9d4f-8fce3de1754a/warrior.jpg`
+        );
+    });
+
+    it(`Custom asset domain should be set in Rich Text element`, () => {
+        const richTextElement = item.elements.plot;
+
+        for (const image of richTextElement.images) {
+            const imagePathname = deliveryUrlHelper.getUrlPathname(image.url);
+            expect(image.url).toEqual(`${customDomain}${imagePathname}`);
+        }
+    });
+
+    it(`Custom asset domain should be set in HTML of Rich Text elements`, () => {
+        const richTextElement = item.elements.plot;
+
+        for (const image of richTextElement.images) {
+            expect(richTextElement.value).toContain(image.url);
+        }
+    });
+});
