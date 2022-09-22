151f759d47f5ce2128a00e991469eef4fa97cf1d
Benoit Morana <benoit@luminary.com>
2022-09-23 08:49:39 +1000

    test: ensure we check order of modular items in the raw response inside the tests

--- test/browser/isolated-tests/elements/rich-text/rich-text.spec.ts
+++ test/browser/isolated-tests/elements/rich-text/rich-text.spec.ts
@@ -9,12 +9,14 @@ describe('Rich-text element', () => {
     });
 
     it(`Rich-text linked items should be re-ordered if not ordered in raw response`, async () => {
+        const rawItem = responseJson.items.find(({ system: { codename } }) => codename === 'rich_text_item_unordered');
         const item = response.items.find(
             ({ system: { codename } }) => codename === 'rich_text_item_unordered'
         ) as IContentItem;
         const element = item.elements.rich as Elements.RichTextElement;
 
         expect(element.type).toEqual(ElementType.RichText);
+        expect(rawItem?.elements.rich.modular_content).toEqual(['n2', 'n4', 'n3', 'n1']);
         expect(element.linkedItemCodenames).toEqual(['n1', 'n2', 'n3', 'n4']);
         for (var i = 0; i < element.linkedItems.length; ++i) {
             expect(element.linkedItems[i].system.codename).toBe(`n${i + 1}`);
@@ -22,12 +24,14 @@ describe('Rich-text element', () => {
     });
 
     it(`Rich-text linked items order should be maintained if already ordered in raw response`, () => {
+        const rawItem = responseJson.items.find(({ system: { codename } }) => codename === 'rich_text_item_ordered');
         const item = response.items.find(
             ({ system: { codename } }) => codename === 'rich_text_item_ordered'
         ) as IContentItem;
         const element = item.elements.rich as Elements.RichTextElement;
 
         expect(element.type).toEqual(ElementType.RichText);
+        expect(rawItem?.elements.rich.modular_content).toEqual(['n1', 'n2', 'n3', 'n4']);
         expect(element.linkedItemCodenames).toEqual(['n1', 'n2', 'n3', 'n4']);
         for (var i = 0; i < element.linkedItems.length; ++i) {
             expect(element.linkedItems[i].system.codename).toBe(`n${i + 1}`);
