e62d261356112a6378d09399ed2b8cf67e866f1c
Richard Sustek <ennspassion@gmail.com>
2023-05-16 11:53:42 +0200

    fix: Preserves linked items order when using items-feed with item mapping

--- lib/query/items-feed/items-feed-query.class.ts
+++ lib/query/items-feed/items-feed-query.class.ts
@@ -148,47 +148,76 @@ export class ItemsFeedQuery<TContentItem extends IContentItem = IContentItem> ex
         }
         // process main items
         for (const item of allContentItems) {
+            if (item.system.codename === 'get_started_with_kontent_ai') {
+                console.log('mapping test item', item);
+                console.log(
+                    'original items',
+                    (item.elements.subpages as any).linkedItems.map((m: any) => m.system.codename)
+                );
+            }
+
             for (const elementKey of Object.keys(item.elements)) {
                 const element = item.elements[elementKey];
 
                 if (element.type === ElementType.ModularContent) {
                     const linkedItemElement = element as Elements.LinkedItemsElement;
 
+                    if (item.system.codename === 'get_started_with_kontent_ai') {
+                        console.log('mapping test element', linkedItemElement);
+                    }
+
+                    // We create separate array for ordered items because the 'linkedItems' from response might be incomplete
+                    // e.g. If 4 items are linked, only 2 might be available in the response. Rest needs to be mapped from all available items
+                    const orderedLinkedItems: IContentItem[] = [];
+
                     for (const linkedItemCodename of linkedItemElement.value) {
-                        if (
-                            !linkedItemElement.linkedItems.find(
-                                (m) => m.system.codename.toLowerCase() === linkedItemCodename.toLowerCase()
-                            )
-                        ) {
-                            const linkedItem = allContentItems.find(
+                        let linkedItem: IContentItem | undefined;
+
+                        const linkedItemInElement = linkedItemElement.linkedItems.find(
+                            (m) => m.system.codename.toLowerCase() === linkedItemCodename.toLowerCase()
+                        );
+                        if (linkedItemInElement) {
+                            linkedItem = linkedItemInElement;
+                        } else {
+                            linkedItem = allContentItems.find(
                                 (m) => m.system.codename.toLowerCase() === linkedItemCodename.toLowerCase()
                             );
+                        }
 
-                            if (linkedItem) {
-                                linkedItemElement.linkedItems.push(linkedItem);
-                            }
+                        if (linkedItem) {
+                            orderedLinkedItems.push(linkedItem);
                         }
                     }
+
+                    // Replace linked items with the ordered one
+                    linkedItemElement.linkedItems = orderedLinkedItems;
                 }
 
                 if (element.type === ElementType.RichText) {
+                    const orderedLinkedItems: IContentItem[] = [];
+
                     const richTextElement = element as Elements.RichTextElement;
 
                     for (const linkedItemCodename of richTextElement.linkedItemCodenames) {
-                        if (
-                            !richTextElement.linkedItems.find(
-                                (m) => m.system.codename.toLowerCase() === linkedItemCodename.toLowerCase()
-                            )
-                        ) {
-                            const linkedItem = allContentItems.find(
+                        let linkedItem: IContentItem | undefined;
+
+                        const linkedItemInElement = richTextElement.linkedItems.find(
+                            (m) => m.system.codename.toLowerCase() === linkedItemCodename.toLowerCase()
+                        );
+                        if (linkedItemInElement) {
+                            linkedItem = linkedItemInElement;
+                        } else {
+                            linkedItem = allContentItems.find(
                                 (m) => m.system.codename.toLowerCase() === linkedItemCodename.toLowerCase()
                             );
+                        }
 
-                            if (linkedItem) {
-                                richTextElement.linkedItems.push(linkedItem);
-                            }
+                        if (linkedItem) {
+                            orderedLinkedItems.push(linkedItem);
                         }
                     }
+
+                    richTextElement.linkedItems = orderedLinkedItems;
                 }
             }
         }
