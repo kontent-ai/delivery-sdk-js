8c894afde62ee2ed40b44fbebc9dd9b8891115c5
Richard Sustek <ennspassion@gmail.com>
2024-09-09 15:48:39 +0200

    feat: Adds optional generic types to `IContentItem` narrowing down available values of system attributes

--- test/browser/setup/models.ts
+++ test/browser/setup/models.ts
@@ -1,21 +1,40 @@
 import { Elements, IContentItem } from '../../../lib';
 
-export type Actor = IContentItem<{
-    first_name: Elements.TextElement;
-    last_name: Elements.TextElement;
-    photo: Elements.AssetsElement;
-}>;
+type LanguageCodenames = 'default' | 'en';
+type CollectionCodenames = 'default';
+type WorkflowCodenames = 'default';
+type WorkflowStepCodenames = 'draft' | 'published' | 'review';
+
+export type Actor = IContentItem<
+    {
+        readonly first_name: Elements.TextElement;
+        readonly last_name: Elements.TextElement;
+        readonly photo: Elements.AssetsElement;
+    },
+    'actor',
+    LanguageCodenames,
+    CollectionCodenames,
+    WorkflowCodenames,
+    WorkflowStepCodenames
+>;
 
 export type releasecategoryTaxonomy = 'global_release' | 'us_only' | 'local_release';
 
-export type Movie = IContentItem<{
-    title: Elements.TextElement;
-    plot: Elements.RichTextElement;
-    released: Elements.DateTimeElement;
-    length: Elements.NumberElement;
-    poster: Elements.AssetsElement;
-    category: Elements.MultipleChoiceElement;
-    stars: Elements.LinkedItemsElement<Actor>;
-    seoname: Elements.UrlSlugElement;
-    releasecategory: Elements.TaxonomyElement<releasecategoryTaxonomy>;
-}>;
+export type Movie = IContentItem<
+    {
+        readonly title: Elements.TextElement;
+        readonly plot: Elements.RichTextElement;
+        readonly released: Elements.DateTimeElement;
+        readonly length: Elements.NumberElement;
+        readonly poster: Elements.AssetsElement;
+        readonly category: Elements.MultipleChoiceElement;
+        readonly stars: Elements.LinkedItemsElement<Actor>;
+        readonly seoname: Elements.UrlSlugElement;
+        readonly releasecategory: Elements.TaxonomyElement<releasecategoryTaxonomy>;
+    },
+    'movie',
+    LanguageCodenames,
+    CollectionCodenames,
+    WorkflowCodenames,
+    WorkflowStepCodenames
+>;
