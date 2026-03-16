901687f944ed28e44a2f47ba43a329442d6763e9
Richard Sustek <ennspassion@gmail.com>
2026-03-16 16:22:22 +0100

    Adds paging utils test

---
+++ tests/unit/paging/paging.utils.spec.ts
@@ -0,0 +1,32 @@
+import type { QueryResponse } from "@kontent-ai/core-sdk";
+import { describe, expect, it } from "vitest";
+import type { PaginationSchema } from "../../../lib/models/core.models.js";
+import { getNextPageByUrl } from "../../../lib/utils/paging.utils.js";
+
+describe("Paging utils", () => {
+	it("Next page URL should be returned", () => {
+		const response: QueryResponse<PaginationSchema, unknown> = {
+			meta: {
+				status: 200,
+				responseHeaders: [],
+				url: "https://domain.com",
+				continuationToken: undefined,
+			},
+			payload: {
+				pagination: {
+					skip: 0,
+					limit: 10,
+					count: 100,
+					next_page: "https://domain.com/next",
+				},
+			},
+		};
+
+		const result = getNextPageByUrl<PaginationSchema, unknown>()(response);
+
+		expect(result).toEqual({
+			continuationToken: undefined,
+			nextPageUrl: "https://domain.com/next",
+		});
+	});
+});
