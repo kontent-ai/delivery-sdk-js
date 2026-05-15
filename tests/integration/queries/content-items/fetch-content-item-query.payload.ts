eb015e1798cace84277c1cf5f5e59f8f1dc4c4de
Richard Sustek <ennspassion@gmail.com>
2026-05-15 10:14:42 +0200

    extends unit payloads

--- tests/integration/queries/content-items/fetch-content-item-query.payload.ts
+++ tests/integration/queries/content-items/fetch-content-item-query.payload.ts
@@ -4,29 +4,166 @@ import type { FetchContentItemPayload } from "../../../../lib/queries/content-it
 export default {
 	item: {
 		system: {
-			id: "d7862a9a-0333-491c-9db9-ee91a7828a84",
-			name: "Brad Pitt",
-			codename: "brad_pitt",
+			id: "21586dc9-9c04-4c9e-a9e4-83b8cd65baef",
+			name: "The Dark Knight",
+			codename: "the_dark_knight",
 			language: "default",
-			type: "star",
+			type: "movie",
 			collection: "default",
 			sitemap_locations: [],
-			last_modified: "2026-04-07T10:20:15.7816154Z",
+			last_modified: "2026-05-04T10:30:48.5100633Z",
 			workflow: "default",
 			workflow_step: "published",
 		},
 		elements: {
-			firstname: {
-				type: "text",
-				name: "Firstname",
-				value: "Brad",
+			just_a_number_and_required: {
+				type: "number",
+				name: "just a number and required",
+				value: null,
 			},
-			lastname: {
+			title: {
 				type: "text",
-				name: "Lastname",
-				value: "Pitt",
+				name: "Title",
+				value: "The Dark Knight",
+			},
+			length_in_minutes: {
+				type: "number",
+				name: "Length in minutes",
+				value: 152,
+			},
+			description: {
+				type: "rich_text",
+				name: "Description",
+				images: {},
+				links: {},
+				modular_content: [],
+				value: "<p>Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, he sets out to dismantle the remaining criminal organizations that plague Gotham City. But the emergence of the Joker creates chaos.</p>",
+			},
+			is_blockbuster: {
+				type: "multiple_choice",
+				name: "Is blockbuster",
+				value: [
+					{
+						name: "yes",
+						codename: "yes",
+					},
+				],
+			},
+			release_date: {
+				type: "date_time",
+				name: "Release date",
+				value: "2008-07-18T00:00:00Z",
+				display_timezone: null,
+			},
+			cover: {
+				type: "asset",
+				name: "Cover",
+				value: [
+					{
+						name: "The_Dark_Knight_(2008_film).jpg",
+						description: null,
+						type: "image/jpeg",
+						size: 52188,
+						url: "https://assets-eu-01.kc-usercontent.com:443/6bdd84fc-f71e-01a0-0ba6-79ce07af7f9b/1ee1e759-4af3-4f0a-acfc-b61a88c1eafe/The_Dark_Knight_%282008_film%29.jpg",
+						width: 259,
+						height: 383,
+						renditions: {
+							default: {
+								rendition_id: "467ed3c4-6c7a-4cb3-908b-f908f7b1e7dd",
+								preset_id: "a6d98cd5-8b2c-4e50-99c9-15192bce2490",
+								width: 150,
+								height: 150,
+								query: "w=150&h=150&fit=clip&rect=54,122,150,150",
+							},
+						},
+					},
+				],
+			},
+			category: {
+				type: "taxonomy",
+				name: "Category",
+				taxonomy_group: "movie_type",
+				value: [
+					{
+						name: "Action",
+						codename: "action",
+					},
+				],
+			},
+			url: {
+				type: "url_slug",
+				name: "Url",
+				value: "the-dark-knight",
+			},
+			metadata__metadata_description: {
+				type: "text",
+				name: "Metadata description",
+				value: "Christopher Nolan's The Dark Knight is widely regarded as one of the greatest superhero films ever made.",
+			},
+			cast: {
+				type: "modular_content",
+				name: "Cast",
+				value: ["christian_bale", "gary_oldman"],
+			},
+			custom: {
+				type: "custom",
+				name: "Custom",
+				value: null,
 			},
 		},
 	},
-	modular_content: {},
+	modular_content: {
+		christian_bale: {
+			system: {
+				id: "2630c0c1-f25a-46df-9dd2-f258ff0d3c60",
+				name: "Christian Bale",
+				codename: "christian_bale",
+				language: "default",
+				type: "star",
+				collection: "default",
+				sitemap_locations: [],
+				last_modified: "2026-04-07T10:20:23.6060976Z",
+				workflow: "default",
+				workflow_step: "published",
+			},
+			elements: {
+				firstname: {
+					type: "text",
+					name: "Firstname",
+					value: "Christian",
+				},
+				lastname: {
+					type: "text",
+					name: "Lastname",
+					value: "Bale",
+				},
+			},
+		},
+		gary_oldman: {
+			system: {
+				id: "55b225d8-9d62-4306-9a8f-16feea53a3ac",
+				name: "Gary Oldman",
+				codename: "gary_oldman",
+				language: "default",
+				type: "star",
+				collection: "default",
+				sitemap_locations: [],
+				last_modified: "2026-04-07T10:23:57.9327818Z",
+				workflow: "default",
+				workflow_step: "published",
+			},
+			elements: {
+				firstname: {
+					type: "text",
+					name: "Firstname",
+					value: "Gary",
+				},
+				lastname: {
+					type: "text",
+					name: "Lastname",
+					value: "Oldman",
+				},
+			},
+		},
+	},
 } satisfies FetchContentItemPayload<DeliveryClientSchema>;
--- tests/integration/queries/content-items/list-content-items-query.payload.ts
+++ tests/integration/queries/content-items/list-content-items-query.payload.ts
@@ -5,40 +5,198 @@ export default {
 	items: [
 		{
 			system: {
-				id: "d56bff34-5ce8-4ac1-9cb8-547848b3a22b",
-				name: "Al Pacino",
-				codename: "al_pacino",
+				id: "f87989d8-ac15-4b87-b43a-37d85e0002f6",
+				name: "Avengers: Endgame",
+				codename: "avengers__endgame",
 				language: "default",
-				type: "star",
+				type: "movie",
 				collection: "default",
 				sitemap_locations: [],
-				last_modified: "2026-04-07T10:20:25.6499619Z",
+				last_modified: "2026-04-07T10:25:39.8815857Z",
 				workflow: "default",
 				workflow_step: "published",
 			},
 			elements: {
-				firstname: {
-					type: "text",
-					name: "Firstname",
-					value: "Al",
+				just_a_number_and_required: {
+					type: "number",
+					name: "just a number and required",
+					value: null,
 				},
-				lastname: {
+				title: {
 					type: "text",
-					name: "Lastname",
-					value: "Pacino",
+					name: "Title",
+					value: "Avengers: Endgame",
+				},
+				length_in_minutes: {
+					type: "number",
+					name: "Length in minutes",
+					value: 181,
+				},
+				description: {
+					type: "rich_text",
+					name: "Description",
+					images: {},
+					links: {},
+					modular_content: [],
+					value: "<p>After the devastating events of Avengers: Infinity War, the remaining Avengers assemble one final time to undo Thanos' destruction and restore the universe. The epic conclusion to the Infinity Saga.</p>",
+				},
+				is_blockbuster: {
+					type: "multiple_choice",
+					name: "Is blockbuster",
+					value: [
+						{
+							name: "yes",
+							codename: "yes",
+						},
+					],
+				},
+				release_date: {
+					type: "date_time",
+					name: "Release date",
+					value: "2019-04-26T00:00:00Z",
+					display_timezone: null,
+				},
+				cover: {
+					type: "asset",
+					name: "Cover",
+					value: [],
+				},
+				category: {
+					type: "taxonomy",
+					name: "Category",
+					taxonomy_group: "movie_type",
+					value: [
+						{
+							name: "Action",
+							codename: "action",
+						},
+					],
+				},
+				url: {
+					type: "url_slug",
+					name: "Url",
+					value: "avengers-endgame",
+				},
+				metadata__metadata_description: {
+					type: "text",
+					name: "Metadata description",
+					value: "The highest-grossing film of all time upon release, Avengers: Endgame is the culmination of 22 Marvel Cinematic Universe films.",
+				},
+				cast: {
+					type: "modular_content",
+					name: "Cast",
+					value: ["chris_evans", "scarlett_johansson", "robert_downey_jr_", "jeremy_renner"],
+				},
+				custom: {
+					type: "custom",
+					name: "Custom",
+					value: null,
 				},
 			},
 		},
 		{
 			system: {
-				id: "b25e6ed1-0a3f-4975-bb84-4c7efefa70d0",
-				name: "Amy Adams",
-				codename: "amy_adams",
+				id: "292468fe-ac5a-4505-b6f1-83cbbee89687",
+				name: "Black Panther",
+				codename: "black_panther",
+				language: "default",
+				type: "movie",
+				collection: "default",
+				sitemap_locations: [],
+				last_modified: "2026-04-07T10:25:53.6784162Z",
+				workflow: "default",
+				workflow_step: "published",
+			},
+			elements: {
+				just_a_number_and_required: {
+					type: "number",
+					name: "just a number and required",
+					value: null,
+				},
+				title: {
+					type: "text",
+					name: "Title",
+					value: "Black Panther",
+				},
+				length_in_minutes: {
+					type: "number",
+					name: "Length in minutes",
+					value: 134,
+				},
+				description: {
+					type: "rich_text",
+					name: "Description",
+					images: {},
+					links: {},
+					modular_content: [],
+					value: "<p>T'Challa returns home to the African nation of Wakanda to take his rightful place as king. When a powerful enemy with ties to his past threatens to destroy the country, he must rally his allies and release the full power of the Black Panther.</p>",
+				},
+				is_blockbuster: {
+					type: "multiple_choice",
+					name: "Is blockbuster",
+					value: [
+						{
+							name: "yes",
+							codename: "yes",
+						},
+					],
+				},
+				release_date: {
+					type: "date_time",
+					name: "Release date",
+					value: "2018-02-16T00:00:00Z",
+					display_timezone: null,
+				},
+				cover: {
+					type: "asset",
+					name: "Cover",
+					value: [],
+				},
+				category: {
+					type: "taxonomy",
+					name: "Category",
+					taxonomy_group: "movie_type",
+					value: [
+						{
+							name: "Action",
+							codename: "action",
+						},
+					],
+				},
+				url: {
+					type: "url_slug",
+					name: "Url",
+					value: "black-panther",
+				},
+				metadata__metadata_description: {
+					type: "text",
+					name: "Metadata description",
+					value: "Marvel's Black Panther became a cultural phenomenon and was nominated for 7 Academy Awards including Best Picture.",
+				},
+				cast: {
+					type: "modular_content",
+					name: "Cast",
+					value: ["chadwick_boseman", "lupita_nyong_o"],
+				},
+				custom: {
+					type: "custom",
+					name: "Custom",
+					value: null,
+				},
+			},
+		},
+	],
+	modular_content: {
+		chadwick_boseman: {
+			system: {
+				id: "831a6311-5e6a-40ee-bb2e-a1a51439b1e1",
+				name: "Chadwick Boseman",
+				codename: "chadwick_boseman",
 				language: "default",
 				type: "star",
 				collection: "default",
 				sitemap_locations: [],
-				last_modified: "2026-04-07T10:21:17.8650469Z",
+				last_modified: "2026-04-07T10:21:12.1866334Z",
 				workflow: "default",
 				workflow_step: "published",
 			},
@@ -46,21 +204,150 @@ export default {
 				firstname: {
 					type: "text",
 					name: "Firstname",
-					value: "Amy",
+					value: "Chadwick",
 				},
 				lastname: {
 					type: "text",
 					name: "Lastname",
-					value: "Adams",
+					value: "Boseman",
 				},
 			},
 		},
-	],
-	modular_content: {},
+		chris_evans: {
+			system: {
+				id: "7a6f289c-3c2f-4b8d-a66e-f7361e8a2a6f",
+				name: "Chris Evans",
+				codename: "chris_evans",
+				language: "default",
+				type: "star",
+				collection: "default",
+				sitemap_locations: [],
+				last_modified: "2026-04-07T10:21:20.9296898Z",
+				workflow: "default",
+				workflow_step: "published",
+			},
+			elements: {
+				firstname: {
+					type: "text",
+					name: "Firstname",
+					value: "Chris",
+				},
+				lastname: {
+					type: "text",
+					name: "Lastname",
+					value: "Evans",
+				},
+			},
+		},
+		jeremy_renner: {
+			system: {
+				id: "d87ce611-4f91-451e-8fa2-24f0922ba71c",
+				name: "Jeremy Renner",
+				codename: "jeremy_renner",
+				language: "default",
+				type: "star",
+				collection: "default",
+				sitemap_locations: [],
+				last_modified: "2026-04-07T10:21:55.7508643Z",
+				workflow: "default",
+				workflow_step: "published",
+			},
+			elements: {
+				firstname: {
+					type: "text",
+					name: "Firstname",
+					value: "Jeremy",
+				},
+				lastname: {
+					type: "text",
+					name: "Lastname",
+					value: "Renner",
+				},
+			},
+		},
+		lupita_nyong_o: {
+			system: {
+				id: "2c71df49-6390-453f-b2dd-12467406b350",
+				name: "Lupita Nyong'o",
+				codename: "lupita_nyong_o",
+				language: "default",
+				type: "star",
+				collection: "default",
+				sitemap_locations: [],
+				last_modified: "2026-04-07T10:21:13.5243957Z",
+				workflow: "default",
+				workflow_step: "published",
+			},
+			elements: {
+				firstname: {
+					type: "text",
+					name: "Firstname",
+					value: "Lupita",
+				},
+				lastname: {
+					type: "text",
+					name: "Lastname",
+					value: "Nyong'o",
+				},
+			},
+		},
+		robert_downey_jr_: {
+			system: {
+				id: "bbc14c55-9d5b-4c75-a968-6e17799987a7",
+				name: "Robert Downey Jr.",
+				codename: "robert_downey_jr_",
+				language: "default",
+				type: "star",
+				collection: "default",
+				sitemap_locations: [],
+				last_modified: "2026-04-07T10:22:45.6740229Z",
+				workflow: "default",
+				workflow_step: "published",
+			},
+			elements: {
+				firstname: {
+					type: "text",
+					name: "Firstname",
+					value: "Robert",
+				},
+				lastname: {
+					type: "text",
+					name: "Lastname",
+					value: "Downey Jr.",
+				},
+			},
+		},
+		scarlett_johansson: {
+			system: {
+				id: "70c23859-845e-40d6-86f7-2c1865ff828d",
+				name: "Scarlett Johansson",
+				codename: "scarlett_johansson",
+				language: "default",
+				type: "star",
+				collection: "default",
+				sitemap_locations: [],
+				last_modified: "2026-04-07T10:20:20.2541643Z",
+				workflow: "default",
+				workflow_step: "published",
+			},
+			elements: {
+				firstname: {
+					type: "text",
+					name: "Firstname",
+					value: "Scarlett",
+				},
+				lastname: {
+					type: "text",
+					name: "Lastname",
+					value: "Johansson",
+				},
+			},
+		},
+	},
 	pagination: {
 		skip: 0,
 		limit: 2,
 		count: 2,
-		next_page: "",
+		next_page: "https://deliver.kontent.ai/6bdd84fc-f71e-01a0-0ba6-79ce07af7f9b/items?system.type=movie&limit=2&skip=2",
 	},
 } satisfies ListContentItemsPayload<DeliveryClientSchema>;
