f4b9e13cc2e830790e75258a6de38be121853cfc
Richard Sustek <ennspassion@gmail.com>
2026-05-15 09:05:25 +0200

    content item transform performance improvements

--- lib/transforms/content-item-transforms.ts
+++ lib/transforms/content-item-transforms.ts
@@ -1,25 +1,61 @@
-import { isDefined } from "@kontent-ai/core-sdk";
 import { match } from "ts-pattern";
 import type { DeliveryClientSchema } from "../models/core.models.js";
 import type { ContentItemPayload, ContentItemPayloadExtended } from "../queries/content-items/models/content-item.models.js";
-import type { ContentItemElementPayloadExtended } from "../queries/content-items/models/element.models.js";
+import type { ContentItemElementPayload, ContentItemElementPayloadExtended } from "../queries/content-items/models/element.models.js";
 
-function extractToMapByCodename<TSchema extends DeliveryClientSchema>({
+type IntermediateRecord<TSchema extends DeliveryClientSchema> = {
+	readonly item: ContentItemPayload<TSchema>;
+	readonly preparedItem: ContentItemPayloadExtended<TSchema>;
+};
+
+function getIntermediateMap<TSchema extends DeliveryClientSchema>({
 	items,
 	modular_content,
 }: {
 	readonly items: readonly ContentItemPayload<TSchema>[];
 	readonly modular_content: Readonly<Record<string, ContentItemPayload<TSchema>>>;
-}) {
-	const itemsFromModularContent = new Map<string, ContentItemPayload<TSchema>>(
-		Object.entries(modular_content).map(([codename, item]) => {
-			return [codename, item];
-		}),
+}): Map<string, IntermediateRecord<TSchema>> {
+	const createIntermediateRecord = <TSchema extends DeliveryClientSchema>(
+		item: ContentItemPayload<TSchema>,
+	): IntermediateRecord<TSchema> => ({ item, preparedItem: { system: item.system, elements: {} } });
+
+	const modularContentRecords = Object.entries(modular_content).reduce<Map<string, IntermediateRecord<TSchema>>>(
+		(map, [key, item]) => map.set(key, createIntermediateRecord(item)),
+		new Map<string, IntermediateRecord<TSchema>>(),
 	);
+	return items.reduce<Map<string, IntermediateRecord<TSchema>>>(
+		(map, item) => map.set(item.system.codename, createIntermediateRecord(item)),
+		modularContentRecords,
+	);
+}
 
-	const mainItems = new Map<string, ContentItemPayload<TSchema>>(items.map((m) => [m.system.codename, m]));
+function resolveExtendedElement<TSchema extends DeliveryClientSchema>(
+	element: ContentItemElementPayload,
+	intermediateRecords: Readonly<Map<string, IntermediateRecord<TSchema>>>,
+): ContentItemElementPayloadExtended {
+	const resolveCodenamesToPreparedItems = <TSchema extends DeliveryClientSchema>(
+		codenames: readonly string[],
+		intermediateRecords: Readonly<Map<string, IntermediateRecord<TSchema>>>,
+	): readonly ContentItemPayloadExtended<TSchema>[] =>
+		codenames.reduce<ContentItemPayloadExtended<TSchema>[]>((preparedItems, codename) => {
+			const record = intermediateRecords.get(codename);
+			if (record) {
+				preparedItems.push(record.preparedItem);
+			}
+			return preparedItems;
+		}, []);
 
-	return new Map<string, ContentItemPayload<TSchema>>([...itemsFromModularContent, ...mainItems]);
+	return match(element)
+		.returnType<ContentItemElementPayloadExtended>()
+		.with({ type: "modular_content" }, (linkedItemElement) => ({
+			...linkedItemElement,
+			items: resolveCodenamesToPreparedItems(linkedItemElement.value, intermediateRecords),
+		}))
+		.with({ type: "rich_text" }, (richTextElement) => ({
+			...richTextElement,
+			items: resolveCodenamesToPreparedItems(richTextElement.modular_content, intermediateRecords),
+		}))
+		.otherwise((element) => element);
 }
 
 export function resolveExtendedItems<TSchema extends DeliveryClientSchema>({
@@ -32,43 +68,35 @@ export function resolveExtendedItems<TSchema extends DeliveryClientSchema>({
 	readonly extendedItems: readonly ContentItemPayloadExtended<TSchema>[];
 	readonly extendedModularContent: Readonly<Record<string, ContentItemPayloadExtended<TSchema>>>;
 } {
-	const allInputItems = extractToMapByCodename({
+	const intermediateRecords = getIntermediateMap({
 		items: inputItems,
 		modular_content: modularContent,
 	});
-	const preparedItems = new Map<string, ContentItemPayloadExtended<TSchema>>(
-		allInputItems.entries().map(([codename, item]) => [codename, { system: item.system, elements: {} }]),
-	);
 
-	for (const item of allInputItems.values()) {
-		const preparedItem = preparedItems.get(item.system.codename);
-
-		if (!preparedItem) {
-			throw new Error(
-				`Item with codename ${item.system.codename} was not prepared for extension. This is a SDK related issue. Please, report it to the developers.`,
-			);
-		}
-
-		for (const [key, element] of Object.entries(item.elements)) {
-			const extendedElement = match(element)
-				.returnType<ContentItemElementPayloadExtended>()
-				.with({ type: "modular_content" }, (linkedItemElement) => ({
-					...linkedItemElement,
-					items: linkedItemElement.value.map((itemCodename) => preparedItems.get(itemCodename)).filter(isDefined),
-				}))
-				.with({ type: "rich_text" }, (richTextElement) => ({
-					...richTextElement,
-					items: richTextElement.modular_content.map((itemCodename) => preparedItems.get(itemCodename)).filter(isDefined),
-				}))
-				.otherwise((element) => element);
-
-			preparedItem.elements[key] = extendedElement;
+	for (const intermediateRecord of intermediateRecords.values()) {
+		for (const [key, element] of Object.entries(intermediateRecord.item.elements)) {
+			intermediateRecord.preparedItem.elements[key] = resolveExtendedElement(element, intermediateRecords);
 		}
 	}
 
 	return {
-		extendedItems: inputItems.map((item) => preparedItems.get(item.system.codename)).filter(isDefined),
+		extendedItems: inputItems.reduce<ContentItemPayloadExtended<TSchema>[]>((items, item) => {
+			const intermediateRecord = intermediateRecords.get(item.system.codename);
+			if (intermediateRecord) {
+				items.push(intermediateRecord.preparedItem);
+			}
+			return items;
+		}, []),
 		// filter only items that were initially present in the modular_content
-		extendedModularContent: Object.fromEntries(preparedItems.entries().filter(([key]) => modularContent[key])),
+		extendedModularContent: Object.keys(modularContent).reduce<Record<string, ContentItemPayloadExtended<TSchema>>>(
+			(extendedModularContent, key) => {
+				const record = intermediateRecords.get(key);
+				if (record) {
+					extendedModularContent[key] = record.preparedItem;
+				}
+				return extendedModularContent;
+			},
+			{},
+		),
 	};
 }
