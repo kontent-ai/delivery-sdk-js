4feb267a25ebbdb0deeb038c0f90cd0a375b2acf
Richard Sustek <ennspassion@gmail.com>
2026-05-18 12:21:08 +0200

    Prepares delivery integration test schemas

---
+++ tests/integration/models/integration-test.schema.ts
@@ -0,0 +1,46 @@
+import type { ContentItemOf, DeliveryClientSchema, ElementType } from "../../../lib/public_api.js";
+
+export type IntegrationTestProjectSchema = DeliveryClientSchema<{
+	readonly languageCodenames: readonly ["default"];
+	readonly taxonomyCodenames: readonly ["movie_type"];
+	readonly contentTypeCodenames: readonly ["movie", "star"];
+	readonly elementCodenames: readonly [
+		"title",
+		"length_in_minutes",
+		"description",
+		"is_blockbuster",
+		"release_date",
+		"cover",
+		"category",
+		"url",
+		"metadata__metadata_description",
+		"cast",
+		"firstname",
+		"lastname",
+	];
+	readonly collectionCodenames: readonly ["default"];
+	readonly workflowCodenames: readonly ["default"];
+	readonly workflowStepCodenames: readonly ["published", "draft"];
+}>;
+
+export type StarElements = {
+	readonly firstname: ElementType.Text;
+	readonly lastname: ElementType.Text;
+};
+
+export type Star = ContentItemOf<IntegrationTestProjectSchema, "star", StarElements>;
+
+export type MovieElements = {
+	readonly title: ElementType.Text;
+	readonly length_in_minutes: ElementType.Number;
+	readonly description: ElementType.RichText;
+	readonly is_blockbuster: ElementType.MultipleChoice<"yes">;
+	readonly release_date: ElementType.DateTime;
+	readonly cover: ElementType.Asset;
+	readonly category: ElementType.Taxonomy;
+	readonly url: ElementType.UrlSlug;
+	readonly metadata__metadata_description: ElementType.Text;
+	readonly cast: ElementType.LinkedItems<Star>;
+};
+
+export type Movie = ContentItemOf<IntegrationTestProjectSchema, "movie", MovieElements>;
