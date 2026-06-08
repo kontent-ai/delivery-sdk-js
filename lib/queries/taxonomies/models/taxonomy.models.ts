ac6c641ee275ef9c7634199033bf5be1cfcce551
Richard Sustek <ennspassion@gmail.com>
2026-06-08 11:06:57 +0200

    Rename taxonomy term generic param

--- lib/queries/taxonomies/models/taxonomy.models.ts
+++ lib/queries/taxonomies/models/taxonomy.models.ts
@@ -2,12 +2,12 @@ import type * as z from "zod/mini";
 import type { DeliveryClientSchema } from "../../../models/core.models.js";
 import type { listTaxonomiesSchema, taxonomySchema, taxonomyTermSchema } from "../schemas/taxonomy.schemas.js";
 
-export type TaxonomyTermPayload<TCodename extends string> = z.infer<ReturnType<typeof taxonomyTermSchema<TCodename>>>;
+export type TaxonomyTermPayload<TTermCodenames extends string> = z.infer<ReturnType<typeof taxonomyTermSchema<TTermCodenames>>>;
 
-export type TaxonomyPayload<TSchema extends DeliveryClientSchema, TCodename extends string> = z.infer<
-	ReturnType<typeof taxonomySchema<TSchema, TCodename>>
+export type TaxonomyPayload<TSchema extends DeliveryClientSchema, TTermCodenames extends string> = z.infer<
+	ReturnType<typeof taxonomySchema<TSchema, TTermCodenames>>
 >;
 
-export type ListTaxonomiesPayload<TSchema extends DeliveryClientSchema, TCodename extends string> = z.infer<
-	ReturnType<typeof listTaxonomiesSchema<TSchema, TCodename>>
+export type ListTaxonomiesPayload<TSchema extends DeliveryClientSchema, TTermCodenames extends string> = z.infer<
+	ReturnType<typeof listTaxonomiesSchema<TSchema, TTermCodenames>>
 >;
--- lib/queries/taxonomies/schemas/taxonomy.schemas.ts
+++ lib/queries/taxonomies/schemas/taxonomy.schemas.ts
@@ -3,16 +3,16 @@ import * as z from "zod/mini";
 import type { DeliveryClientSchema } from "../../../models/core.models.js";
 import { paginationSchema } from "../../../models/pagination.schemas.js";
 
-export const taxonomyTermSchema = <TCodename extends string>() =>
+export const taxonomyTermSchema = <TTermCodenames extends string>() =>
 	z.object({
 		name: z.string(),
-		codename: codenameOf<TCodename>(),
+		codename: codenameOf<TTermCodenames>(),
 		get terms() {
-			return z.readonly(z.array(taxonomyTermSchema<TCodename>()));
+			return z.readonly(z.array(taxonomyTermSchema<TTermCodenames>()));
 		},
 	});
 
-export const taxonomySchema = <TSchema extends DeliveryClientSchema, TCodename extends string>() =>
+export const taxonomySchema = <TSchema extends DeliveryClientSchema, TTermCodenames extends string>() =>
 	z.readonly(
 		z.object({
 			system: z.readonly(
@@ -23,14 +23,14 @@ export const taxonomySchema = <TSchema extends DeliveryClientSchema, TCodename e
 					last_modified: z.iso.datetime(),
 				}),
 			),
-			terms: z.readonly(z.array(taxonomyTermSchema<TCodename>())),
+			terms: z.readonly(z.array(taxonomyTermSchema<TTermCodenames>())),
 		}),
 	);
 
-export const listTaxonomiesSchema = <TSchema extends DeliveryClientSchema, TCodename extends string>() =>
+export const listTaxonomiesSchema = <TSchema extends DeliveryClientSchema, TTermCodenames extends string>() =>
 	z.readonly(
 		z.object({
-			taxonomies: z.readonly(z.array(taxonomySchema<TSchema, TCodename>())),
+			taxonomies: z.readonly(z.array(taxonomySchema<TSchema, TTermCodenames>())),
 			...paginationSchema.shape,
 		}),
 	);
