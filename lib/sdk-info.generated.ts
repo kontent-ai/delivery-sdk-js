96eb402fb0a284efb889af3a1f24c66832f86675
Richard Sustek <ennspassion@gmail.com>
2026-08-18 12:03:41 +0200

    release

--- lib/sdk-info.generated.ts
+++ lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from '@kontent-ai/core-sdk';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '16.4.6',
+    version: '16.4.7',
     name: '@kontent-ai/delivery-sdk'
 };
